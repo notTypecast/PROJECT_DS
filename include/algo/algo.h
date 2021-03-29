ad7c1c9c751c906b1644d87702a19dd295e32437
notTypecast <ynkodizzy@gmail.com>
2021-03-29 20:49:30 +0300

    Implemented mergeSort

--- include/algo/algo.h
+++ include/algo/algo.h
@@ -2,7 +2,8 @@
 #define ALGO_H
 
 namespace algo {
-
+    void merge(int arr[], int left, int middle, int right);
+    void mergeSort(int arr[], int left, int right);
 }
 
 #endif //ALGO_H
---
+++ src/algo/mergeSort.cpp
@@ -0,0 +1,66 @@
+#include "../../include/algo/algo.h"
+
+void algo::merge(int arr[], int left, int middle, int right) {
+    //get size of subarrays
+    int leftArraySize = middle - left + 1;
+    int rightArraySize = right - middle;
+
+    //temp arrays
+    int *tmpLeftArr = new int[leftArraySize];
+    int *tmpRightArr = new int[rightArraySize];
+
+    int i, j;
+
+    //copy data to temp arrays
+    for (i = 0; i < leftArraySize; ++i) {
+        tmpLeftArr[i] = arr[left + i];
+    }
+    for (j = 0; j < rightArraySize; ++j) {
+        tmpRightArr[j] = arr[middle + 1 + j];
+    }
+
+    i = 0; //index of left subarray
+    j = 0; //index of right subarray
+    int k = l; //index of merged array
+
+    while (i < leftArraySize && j < rightArraySize) {
+        if (tmpLeftArr[i] <= tmpRightArr[j]) {
+            arr[k] = tmpLeftArr[i++];
+        }
+        else {
+            arr[k] = tmpRightArr[j++];
+        }
+        ++k;
+    }
+
+    while (i < leftArraySize) {
+        arr[k++] = tmpLeftArr[i++];
+    }
+
+    while (j < rightArraySize) {
+        arr[k++] = tmpRightArr[j++];
+    }
+
+    delete[] tmpLeftArr;
+    delete[] tmpRightArr;
+
+
+}
+
+void algo::mergeSort(int arr[], int left, int right) {
+    //end if left index is >= right index
+    if (left >= right) {
+        return;
+    }
+
+    //get middle index
+    int middle = (right + left) / 2;
+
+    //call mergeSort on left and right subarrays recursively
+    algo::mergeSort(arr, left, middle);
+    algo::mergeSort(arr, middle + 1, right);
+    //merge resulting arrays
+    algo::merge(arr, left, middle, right);
+
+}
+

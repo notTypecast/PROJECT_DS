8655f510279d013ef8da975617654af6b86e4e7c
notTypecast <ynkodizzy@gmail.com>
2021-04-14 14:03:54 +0300

    Changed mergeSort to work with struct array

--- include/algo.h
+++ include/algo.h
@@ -5,8 +5,8 @@
 
 namespace algo {
     //mergeSort
-    void merge(int arr[], int left, int middle, int right);
-    void mergeSort(int arr[], int left, int right);
+    void merge(utils::stock::StockDayData* data, int left, int middle, int right);
+    void mergeSort(utils::stock::StockDayData* data, int left, int right);
     //quickSort
     int partition(int arr[], int left, int right);
     void quickSort(int arr[], int left, int right);
--- src/algo/mergeSort.cpp
+++ src/algo/mergeSort.cpp
@@ -1,54 +1,52 @@
 #include "../../include/algo.h"
 
-void algo::merge(int arr[], int left, int middle, int right) {
+void algo::merge(utils::stock::StockDayData* data, int left, int middle, int right) {
     //get size of subarrays
     int leftArraySize = middle - left + 1;
     int rightArraySize = right - middle;
 
     //temp arrays
-    int *tmpLeftArr = new int[leftArraySize];
-    int *tmpRightArr = new int[rightArraySize];
-
+    utils::stock::StockDayData *tmpLeftArr = new utils::stock::StockDayData[leftArraySize];
+    utils::stock::StockDayData *tmpRightArr = new utils::stock::StockDayData[rightArraySize];
     int i, j;
 
     //copy data to temp arrays
     for (i = 0; i < leftArraySize; ++i) {
-        tmpLeftArr[i] = arr[left + i];
+        tmpLeftArr[i] = data[left + i];
     }
     for (j = 0; j < rightArraySize; ++j) {
-        tmpRightArr[j] = arr[middle + 1 + j];
+        tmpRightArr[j] = data[middle + 1 + j];
     }
 
     i = 0; //index of left subarray
     j = 0; //index of right subarray
     int k = left; //index of merged array
 
-
     while (i < leftArraySize && j < rightArraySize) {
-        if (tmpLeftArr[i] <= tmpRightArr[j]) {
-            arr[k] = tmpLeftArr[i++];
+        // sorting by open value
+        // if open values are equal, then timestamp will be used to sort
+        if (tmpLeftArr[i].open < tmpRightArr[j].open || (tmpLeftArr[i].open == tmpRightArr[j].open && tmpLeftArr[i].timestamp < tmpRightArr[j].timestamp)) {
+            data[k] = tmpLeftArr[i++];
         }
         else {
-            arr[k] = tmpRightArr[j++];
+            data[k] = tmpRightArr[j++];
         }
         ++k;
     }
 
     while (i < leftArraySize) {
-        arr[k++] = tmpLeftArr[i++];
+        data[k++] = tmpLeftArr[i++];
     }
 
     while (j < rightArraySize) {
-        arr[k++] = tmpRightArr[j++];
+        data[k++] = tmpRightArr[j++];
     }
 
     delete[] tmpLeftArr;
     delete[] tmpRightArr;
-
-
 }
 
-void algo::mergeSort(int arr[], int left, int right) {
+void algo::mergeSort(utils::stock::StockDayData* data, int left, int right) {
     //end if left index is >= right index
     if (left >= right) {
         return;
@@ -58,10 +56,10 @@ void algo::mergeSort(int arr[], int left, int right) {
     int middle = (right + left) / 2;
 
     //call mergeSort on left and right subarrays recursively
-    algo::mergeSort(arr, left, middle);
-    algo::mergeSort(arr, middle + 1, right);
+    algo::mergeSort(data, left, middle);
+    algo::mergeSort(data, middle + 1, right);
     //merge resulting arrays
-    algo::merge(arr, left, middle, right);
+    algo::merge(data, left, middle, right);
 
 }
 
